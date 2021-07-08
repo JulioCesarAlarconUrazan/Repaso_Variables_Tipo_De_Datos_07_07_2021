69514876f87dd094962065a97718942cfc357d49
JulioCesarAlarconUrazan <jcalarcon0990@gmail.com>
2021-07-07 20:57:48 -0500

    Repaso de variables 07072021

---
+++ main.js
@@ -0,0 +1,11 @@
+nombreCompleto = `Julio Cesar Alarcon Urazan`;
+edad = 30;
+estudianteComfenalco = true;
+ciudadResidencia = `Floridablanca`;
+celular ="3223456789";
+
+console.log(nombreCompleto, typeof(nombreCompleto) );
+console.log(edad, typeof(edad) );
+console.log(estudianteComfenalco, typeof(estudianteComfenalco) );
+console.log(ciudadResidencia, typeof(ciudadResidencia) );
+console.log(celular, typeof(celular) );
